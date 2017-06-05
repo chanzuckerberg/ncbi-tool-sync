eda2c3588a7e91e9b1cd0215b8982d532e16cd4c
Jonathan Sheu <jsheu@chanzuckerberg.com>
2017-06-05 15:46:18 -0700

    Continue rsync workflow

--- main.go
+++ main.go
@@ -2,37 +2,19 @@ package main
 
 import (
     "fmt"
-    "github.com/jlaffaye/ftp"
-    "bytes"
     "io/ioutil"
     "gopkg.in/yaml.v2"
     "os"
-    "io"
     "strings"
-    //"gopkg.in/dutchcoders/goftp.v1"
     "os/exec"
-    //"log"
     "path/filepath"
     "time"
 )
 
 var config conf
-var ftpClient *ftp.ServerConn
 
 func main() {
-    //var err error
-    //ftpClient, err = connectToServer()
-    //ftpClient.DisableEPSV = false
-    //if err != nil {
-    //    fmt.Println(err)
-    //}
-
-    //err = traverseFolder(config.RemotePath)
-    //if err != nil {
-    //    fmt.Println(err)
-    //}
-
-    config.loadFTPconfig()
+    config.loadConfig()
     //listAllFiles("")
     rsyncSimple("")
 }
@@ -41,13 +23,6 @@ type SyncConfig struct {
     Args     []string
     From     string
     To       string
-}
-
-func trimPath(input string) string {
-    pieces := strings.Split(input, "/")
-    last := pieces[len(pieces)-1]
-    result := input[:len(input)-len(last)]
-    return result
 }
 
 func callCommand(input string) ([]byte, error) {
@@ -61,14 +36,14 @@ func listAllFiles(input string) []string {
     // Call rsync and parse out list of files
     source := fmt.Sprintf("rsync://%s/%s", config.Server, input)
     fmt.Println(source)
-    cmd := fmt.Sprintf("rsync -nr --list-only %s | tail -n+18 | tr -s ' ' | grep -v '^d' | cut -d ' ' -f5", source)
+    cmd := fmt.Sprintf("rsync -anr --list-only --no-motd %s | tail -n+1 | tr -s ' ' | grep -v '^d' | cut -d ' ' -f5", source)
     out, _ := callCommand(cmd)
 
     fileList := strings.Split(string(out[:]), "\n")
-    fileList = fileList[:len(fileList)-1]  // Remove last empty elem
+    fileList = fileList[:len(fileList)-1]       // Remove last empty elem
 
     // Append input path to beginning of each file path output
-    trimmed := trimPath(input)
+    trimmed := filepath.Dir(input)
     var resultList []string
     for _, value := range fileList {
         resultList = append(resultList, trimmed + value)
@@ -76,26 +51,51 @@ func listAllFiles(input string) []string {
     return resultList
 }
 
+func listNewFiles(input string) []string {
+    // Call rsync dry run and parse out list of new files
+    source := fmt.Sprintf("rsync://%s%s", config.Server, input)
+    cmd := fmt.Sprintf("rsync -anrv --ignore-existing --no-motd --exclude='.*' %s %s | tail -n+2 | grep -E -v '/$'", source, config.LocalPath + "/")
+    out, _ := callCommand(cmd)
+
+    fileList := strings.Split(string(out[:]), "\n")
+    fileList = fileList[:len(fileList)-4]       // Remove last junk lines
+
+    // Append input path to beginning of each file path output
+    trimmed := filepath.Dir(input)
+    var resultList []string
+    for _, value := range fileList {
+        resultList = append(resultList, trimmed + "/" + value)
+    }
+    return resultList
+}
+
 func rsyncSimple(input string) {
-    input = "blast/demo"
+    input = "/blast/demo"
 
-    // Call rsync
-    source := fmt.Sprintf("rsync://%s/%s", config.Server, input)
-    changedDir := curTimeName()
-    template := "rsync -abrz --delete --exclude='.*' --backup-dir='%s' %s %s"
-    cmd := fmt.Sprintf(template, changedDir, source, config.LocalPath)
-    _, err := callCommand(cmd)
-    //fmt.Printf("%s %s", out, err)
+    // List only new files
+    listNewFiles(input)
 
-    // Handle changed files
-    dest := fmt.Sprintf("%s/%s", config.LocalPath, changedDir)
+    // Call rsync in backup mode to copy modified and deleted files
+    source := fmt.Sprintf("rsync://%s%s", config.Server, input)
+    tempDir := curTimeName()
+    template := "rsync -abrzv --delete --no-motd --exclude='.*' --backup-dir='%s' %s %s"
+    cmd := fmt.Sprintf(template, tempDir, source, config.LocalPath)
+    fmt.Println(cmd)
+    out, err := callCommand(cmd)
+    fmt.Printf("%s", out)
+
+    // Handle new files
+
+
+    // Handle replaced or deleted files
+    dest := fmt.Sprintf("%s/%s", config.LocalPath, tempDir)
     if _, err := os.Stat(dest); err == nil {
         err = filepath.Walk(dest, visitFile)
     }
     fmt.Printf("filepath.Walk() returned %v\n", err)
 
     // Delete temp folder after handling files
-    
+
 }
 
 func curTimeName() string {
@@ -114,90 +114,6 @@ func visitFile(path string, f os.FileInfo, err error) error {
     return err
 }
 
-func checksumChecks(input string) {
-}
-
-func traverseFolder(path string) error {
-    // Get directory listing
-    entries, err := ftpClient.List(path)
-    if err != nil {
-        return err
-    }
-
-    // Traverse each entry
-    for _, entry := range entries {
-        name := entry.Name
-        if name == "." || name == ".." || string(name[0]) == "." {
-            continue
-        }
-
-        fmt.Println("NAME IS: " + name)
-        newPath := path + "/" + name
-
-        if entry.Type == 0 {            // Is a file
-            err = handleFile(newPath)
-            if err != nil {
-                return err
-            }
-        } else if entry.Type == 1 {     // Is a folder
-            fmt.Println("GOING TO: " + path + "/" + name)
-            traverseFolder(newPath)
-        }
-    }
-
-    return nil
-}
-
-func handleFile(remotePath string) error {
-    localPath := config.LocalPath + remotePath
-    fmt.Println("LOCAL PATH IS: " + localPath)
-    fmt.Println("REMOTE PATH IS: " + remotePath)
-
-    // File exists remotely but not locally
-    if _, err := os.Stat(localPath); os.IsNotExist(err) {
-        fmt.Println("DOES NOT EXIST: " + remotePath)
-        err := downloadFile(remotePath)
-        if err != nil {
-            return err
-        }
-    }
-
-    return nil
-}
-
-func downloadFile(remotePath string) error {
-    //fmt.Println("GOING TO DOWNLOAD: " + remotePath)
-    localPath := config.LocalPath[1:] + remotePath
-    fmt.Println("LOCAL IS: " + localPath)
-    fmt.Println("BLAH")
-    ftpClient.DisableEPSV = false
-    reader, err := ftpClient.Retr(remotePath)
-    if err != nil {
-        fmt.Println(err)
-        panic(err)
-    }
-    fmt.Println("DONE")
-
-    buf := new(bytes.Buffer)
-    buf.ReadFrom(reader)
-    //s := buf.String()
-    //fmt.Println(s)
-    //data := []byte("hello")
-    fo, err := os.Create(localPath)
-    if err != nil {
-        return err
-    }
-    defer fo.Close()
-    reader.Close()
-
-    _, err = io.Copy(fo, strings.NewReader("hello"))
-    if err != nil {
-        return err
-    }
-
-    return err
-}
-
 type conf struct {
     Server     string `yaml:"Server"`
     Port       string `yaml:"Port"`
@@ -207,7 +123,7 @@ type conf struct {
     LocalPath  string `yaml:"LocalPath"`
 }
 
-func (c *conf) loadFTPconfig() *conf {
+func (c *conf) loadConfig() *conf {
     file, err := ioutil.ReadFile("config.yaml")
     if err != nil {
         fmt.Println(err)
