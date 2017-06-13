42984c0be6c4c019d52c2afafecb648a4665a2db
Jonathan Sheu <jsheu@chanzuckerberg.com>
2017-06-13 13:27:17 -0700

    Replace OS modtime call with FTP call

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
     "io"
     "encoding/hex"
     "ncbi_proj/storage"
+    "github.com/jlaffaye/ftp"
 )
 
 type Context struct {
@@ -36,8 +37,14 @@ func main() {
     c.loadConfig()
     c.storHelper.LoadConfig()
     err = c.storHelper.MountFuse()
+    if err != nil {
+        fmt.Println(err)
+    }
+    //defer c.storHelper.UmountFuse()
+
+    //err = c.getServerTime("")
+
     err = c.callRsyncFlow(c.SourcePath)
-    //err := awsTest()
     if err != nil {
         fmt.Println(err)
     }
@@ -163,6 +170,8 @@ func (c *Context) handleNewVersions(files []string) error {
 
 // Handle one file with a new version on disk
 func (c *Context) handleNewVersion(file string) error {
+    var err error
+
     // Set version number
     var versionNum int = 1
     prevNum := c.findPrevVersionNum(file, true)
@@ -171,10 +180,10 @@ func (c *Context) handleNewVersion(file string) error {
     }
 
     // Set datetime modified
-    path := fmt.Sprintf("%s%s", c.LocalTop, file)
-    info, err := os.Stat(path)
-    if err != nil { return err }
-    modTime := fmt.Sprintf("%s", info.ModTime())
+    modTime, err := c.getServerTime(file)
+    if err != nil {
+        return err
+    }
 
     // Insert into database
     query := fmt.Sprintf("insert into entries(PathName, VersionNum, " +
@@ -260,6 +269,43 @@ func generateHash(origPath string, path string, num int) (string, error) {
     hashInBytes := hash.Sum(nil)[:16]
     result = hex.EncodeToString(hashInBytes)
     return result, nil
+}
+
+// Get the date time modified from the FTP server
+func (c *Context) getServerTime(input string) (string, error) {
+    folder := filepath.Dir(input)
+    file := filepath.Base(input)
+
+    // Open FTP connection
+    client, err := c.connectToServer()
+    defer client.Quit()
+    entries, err := client.List(folder)
+    if err != nil {
+        return "", err
+    }
+
+    // Find the right entry
+    for _, entry := range entries {
+        if entry.Name == file {
+            return fmt.Sprintf("%s", entry.Time), err
+        }
+    }
+    return "", err
+}
+
+func (c *Context) connectToServer() (*ftp.ServerConn, error) {
+    addr := c.Server + ":" + c.Port
+    client, err := ftp.Dial(addr)
+    if err != nil {
+        return nil, err
+    }
+
+    err = client.Login(c.Username, c.Password)
+    if err != nil {
+        return nil, err
+    }
+
+    return client, nil
 }
 
 // Load the configuration file
