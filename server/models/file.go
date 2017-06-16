cb4f04a010490c7d92f0ed6976d1d40464db13dc
Jonathan Sheu <jsheu@chanzuckerberg.com>
2017-06-16 14:48:54 -0700

    Finish simple path for serving file from S3

--- server/models/file.go
+++ server/models/file.go
@@ -6,6 +6,9 @@ import (
     "time"
     "github.com/aws/aws-sdk-go/service/s3"
     "ncbi_proj/server/utils"
+    "strconv"
+    "fmt"
+    "database/sql"
 )
 
 type File struct {
@@ -21,34 +24,76 @@ func NewFile() *File {
     return &File{}
 }
 
-func (f *File) Get(pathName string, versionNum int, ctx *utils.Context) (Response, error) {
-    url, err := f.S3KeyToURL(pathName, ctx)
-    if err != nil {
-        return Response{}, err
+func (f *File) Get(pathName string, versionNum string, ctx *utils.Context) (Response, error) {
+    var url string
+    var err error
+    key := pathName
+    resp := Response{}
+
+    // Get archive blob key if version specified
+    if versionNum != "" {
+        num, _ := strconv.Atoi(versionNum)
+        key, err = f.getS3Key(pathName, num, ctx)
+        if err != nil {
+            return resp, err
+        }
     }
-    return Response{pathName, "Placeholder", url}, nil
+
+    url, err = f.S3KeyToURL(key, ctx)
+    if err == nil {
+        resp.Path = pathName
+        resp.ModTime = "Placeholder"
+        resp.Url = url
+    }
+    return resp, err
 }
 
-func (f *File) S3KeyToURL(pathName string, ctx *utils.Context) (string, error) {
-    // Get a presigned temporary URL from S3 for a key
+// Look in database for proper key for specific version
+func (f *File) getS3Key(pathName string, versionNum int, ctx *utils.Context) (string, error) {
+    // Query the database
+    res := ""
+    query := fmt.Sprintf("select * from entries " +
+        "where PathName='%s' and VersionNum=%d", pathName, versionNum)
+    row, err := ctx.Db.Query(query)
+    defer row.Close()
+    if err != nil {
+        return res, err
+    }
+
+    // Process results
+    row.Next()
+    var path string;
+    var version int;
+    var date sql.NullString;
+    var blob sql.NullString;
+
+    err = row.Scan(&path, &version, &date, &blob)
+    if err != nil {
+        // No results at all for this name and version
+        return res, err
+    } else if !blob.Valid {
+        // Entry is there but not archived. Serve latest.
+        return pathName, nil
+    } else {
+        // Success
+        archiveKey, _ := blob.Value()
+        res = fmt.Sprintf("/archive/%s", archiveKey)
+    }
+    return res, err
+}
+
+// Get a pre-signed temporary URL from S3 for a key
+func (f *File) S3KeyToURL(key string, ctx *utils.Context) (string, error) {
+    url := ""
     svc := s3.New(session.New(&aws.Config{
         Region: aws.String("us-west-2"),
     }))
     req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
         Bucket: aws.String(ctx.Bucket),
-        Key:    aws.String(pathName),
+        Key:    aws.String(key),
     })
     url, err := req.Presign(1 * time.Hour)
-
-    if err != nil {
-        return "", err
-    }
-
     return url, err
-}
-
-func (f *File) GetLatest(pathName string) {
-
 }
 
 func (f *File) GetHistory(pathName string) {
